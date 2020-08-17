151cd1c7b66a3dfe7d58604a79253d047fd3453e
duyvl <duyvl@novaon.vn>
2020-08-17 16:18:40 +0700

    init

---
+++ app/src/main/java/com/example/android02/TestActivity.java
@@ -0,0 +1,16 @@
+package com.example.android02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android02.R;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
